12217061601a8c106da4d5d58276462b8534a6e6
Danijel C <danijelc+hyf@gmail.com>
2021-12-03 18:22:56 +0100

    Responsive layout with correct round button

--- src/components/Form.js
+++ src/components/Form.js
@@ -10,40 +10,77 @@ import {
 
 export const createForm = () => {
   return String.raw`
- <form>
-  <div class="mb-3">
-    <label for="amount" class="form-label">Amount</label>
-    <input type="text" class="form-control" id=${AMOUNT_ID} value="1.00" aria-describedby="" />
-  </div>
-  <div class="mb-3">
-    <label class="form-label">From</label>
-    <select id=${FROM_ID} class="form-select" aria-label="Default select example">
-      <option selected>Select a currency</option>
-    </select>
-  </div>
-  <div class="mb-3">
-    <button id=${SWITCH_CURRENCIES_BTN_ID} type="button" class="btn btn-secondary btn-round">
-    <i class="fas fa-exchange-alt fa-rotate-90"></i>
-    </button>
-  </div>
-  <div class="mb-3">
-    <label class="form-label">To</label>
-    <select id=${TO_ID} class="form-select" aria-label="Default select example">
-      <option selected>Select a currency</option>
-    </select>
-  </div>
-  <div class="collapse mb-3" id="collapseResult">
-    <div id=${RESULT_ID} class="card card-body">
+ <form class="mt-3 row shadow p-3 mb-5 bg-body">
+  <div class="mb-3 col-md-3">
+    <label for="amount" class="form-label fw-bold">Amount</label>
+    <div class="input-group input-group-lg">
+      <input
+        type="text"
+        class="form-control form-input-lg"
+        id="${AMOUNT_ID}"
+        value="1.00"
+        aria-describedby=""
+      />
     </div>
   </div>
-  <a
-    href="#"
-    id = ${SUBMIT_BTN_ID}
-    type="button"
-    class="btn btn-primary col-12"
+  <div class="mb-3 col-md-4">
+    <label class="form-label fw-bold">From</label>
+    <select
+      id="${FROM_ID}"
+      class="form-select form-select-lg"
+      aria-label="Default select example"
+    >
+      <option selected>Select a currency</option>
+    </select>
+  </div>
+  <div
+    class="
+      mb-3
+      col-md-1
+      d-flex
+      justify-content-left justify-content-md-center
+      align-items-end
+      btn-container
+    "
   >
-    Submit
-</a>
+    <button
+      id="${SWITCH_CURRENCIES_BTN_ID}"
+      type="button"
+      class="
+        btn btn-secondary btn-lg
+        rounded-circle
+        btn-round
+        d-flex
+        justify-content-center
+        align-items-center
+      "
+    >
+      <i class="fas fa-exchange-alt fa-rotate-90"></i>
+    </button>
+  </div>
+  <div class="mb-3 col-md-4">
+    <label class="form-label fw-bold">To</label>
+    <select
+      id="${TO_ID}"
+      class="form-select form-select-lg"
+      aria-label="Default select example"
+    >
+      <option selected>Select a currency</option>
+    </select>
+  </div>
+  <div class="mb-3 collapse col-12" id="collapseResult">
+    <div id="${RESULT_ID}" class="card card-body"></div>
+  </div>
+  <div class="mb-3 col-12 text-end">
+    <a
+      href="#"
+      id="${SUBMIT_BTN_ID}"
+      type="button"
+      class="btn btn-primary btn-lg col-12 col-md-2"
+    >
+      Submit
+    </a>
+  </div>
 </form>
     `;
 };
